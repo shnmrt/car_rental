4bc72ece8b01444fd36f79c97224fae731a8ecc1
Murat Şahin <sahinmurat3@gmail.com>
2022-06-12 14:00:35 +0300

    styling forms with css

--- mysite/cars/forms.py
+++ mysite/cars/forms.py
@@ -4,5 +4,9 @@ class ReviewForm(forms.Form):
     first_name = forms.CharField(label='First Name', max_length=100)
     last_name = forms.CharField(label='Last Name', max_length=100)
     email = forms.EmailField(label='Email')
-    review = forms.CharField(label='Please write your review here', max_length=250)
+    review = forms.CharField(
+        label='Please write your review here', 
+        max_length=250, 
+        widget=forms.Textarea(attrs={'class':'myform', 'rows':'10','cols':'50'})
+    )
     
